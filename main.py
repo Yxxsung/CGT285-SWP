ecff08ea39c2c6fc6cd859fb7996e21ade534a6c
Sophia Alexander <sophia.alexander@aol.com>
2026-03-06 01:20:21 -0500

    Rect Done, Circle Started

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 #CGT 115
 #Shapes With Pygame Assignment
 
-#Imports the pygame module
+#Imports the pygame module.
+#Had to install in the terminal first using "pip install pygame"
 import pygame
 
 #Initiates the pygame module
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
-rect=pygame.Rect(100,100,200,200)
-pygame.draw.rect(screen,(255,0, 0), rect)
+screen = pygame.display.set_mode((1000, 800))
+rect=pygame.Rect(300,300,150,100)
+pygame.draw.rect(screen,(255,255,255), rect)
+pygame.draw.circle(screen, (255,255,255), (100,100), (100))
 pygame.display.update()
 done = False
 while not done:
