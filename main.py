b3d86024b7cb7148bd1be2cdc2fc632de0fb174d
Sophia Alexander <sophia.alexander@aol.com>
2026-03-05 22:58:18 -0500

    second Upload

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ import pygame
 #Initiates the pygame module
 pygame.init()
 
+screen = pygame.display.set_mode((800, 600))
+rect=pygame.Rect(100,100,200,200)
+pygame.draw.rect(screen,(255,0, 0), rect)
+pygame.display.update()
+done = False
+while not done:
+    events = pygame.event.get()
+for event in events:
+    if event.type == pygame.QUIT:
+        done = True
+        pygame.quit()
